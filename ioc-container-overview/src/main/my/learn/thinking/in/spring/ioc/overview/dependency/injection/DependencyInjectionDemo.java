1ce9b5d10f229890cc8bb76cecf62cd194fc983c
GitJumping <Jack.sTreasure@protonmail.com>
2024-11-22 00:56:55 +0800

    26 | Spring IoC容器：BeanFactory和ApplicationContext谁才是Spring IoC容器？

--- ioc-container-overview/src/main/my/learn/thinking/in/spring/ioc/overview/dependency/injection/DependencyInjectionDemo.java
+++ ioc-container-overview/src/main/my/learn/thinking/in/spring/ioc/overview/dependency/injection/DependencyInjectionDemo.java
@@ -6,10 +6,12 @@ import my.learn.thinking.in.spring.ioc.overview.repository.UserRepository;
 import org.springframework.beans.factory.BeanFactory;
 import org.springframework.beans.factory.ListableBeanFactory;
 import org.springframework.beans.factory.ObjectFactory;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.core.env.Environment;
 
 import java.util.Map;
+import java.util.stream.StreamSupport;
 
 /**
  * 依赖注入实例
@@ -39,11 +41,13 @@ public class DependencyInjectionDemo {
         // 配置 XML 配置文件
         // 启动 Spring 应用上下文
         // 导入实例，因为包含上下文的信息，一个是injection的上下文，一个是lookup的上下文
-        BeanFactory beanFactory = new ClassPathXmlApplicationContext("classpath:META-INF/dependency-injection-context.xml");
+//        BeanFactory beanFactory = new ClassPathXmlApplicationContext("classpath:META-INF/dependency-injection-context.xml");
+        //因为ApplicationContext 就是一个 BeanFactory的子接口，享有BeanFactory原来定义有的方法签名
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("classpath:META-INF/dependency-injection-context.xml");
         /*
         依赖来源1:自定义地bean
          */
-        UserRepository userRepository = beanFactory.getBean("userRepository", UserRepository.class);
+        UserRepository userRepository = applicationContext.getBean("userRepository", UserRepository.class);
         System.out.println(userRepository.getUsers());
 
         /**
@@ -51,7 +55,7 @@ public class DependencyInjectionDemo {
          */
         //依赖注入
         System.out.println(userRepository.getBeanFactory());
-        System.out.println(userRepository.getBeanFactory() == beanFactory);
+        System.out.println(userRepository.getBeanFactory() == applicationContext);
 
         //private ObjectFactory<User> userObjectFactory;下返回superUser
         //依赖注入注入进来的，这里得到的Super，因为定义了primary
@@ -60,7 +64,7 @@ public class DependencyInjectionDemo {
         ObjectFactory userFactory = userRepository.getUserObjectFactory();
         System.out.println(userFactory.getObject());
         //这里相等，说明刚才 ObjectFactory auto-wire时候，注入了一个ApplicationContext，ApplicationContext又是一个BeanFactory
-        System.out.println(userFactory.getObject() == beanFactory);
+        System.out.println(userFactory.getObject() == applicationContext);
 
         //依赖查找（错误）
         /*
@@ -75,8 +79,42 @@ public class DependencyInjectionDemo {
         Spring IoC 底层容器就是指的 BeanFactory 的实现类，大多数情况是 DefaultListableBeanFactory 这个类，它来管理 Spring Beans，而 ApplicationContext 通常为开发人员接触到的 IoC 容器，它是一个 Facade，Wrap 了 BeanFactory 的实现
         自定义的bean和内建的bean能够通过依赖查找查询到，但是内建依赖是不能通过依赖查找查询到，因为他们的来源不同
          */
-        Environment environment = beanFactory.getBean(Environment.class);
+        Environment environment = applicationContext.getBean(Environment.class);
         System.out.println("获取 Environment 类型地 Bean： "+environment);
     }
 
+    /**
+     * https://docs.spring.io/spring-framework/reference/6.0/core/beans/introduction.html
+     *
+     * org.springframework.context.support.AbstractApplicationContext
+     *
+     * org.springframework.context.ConfigurableApplicationContext#getBeanFactory()
+     *
+     * org.springframework.context.ConfigurableApplicationContext <-
+     * org.springframework.context.ApplicationContext <-
+     * org.springframework.beans.factory.BeanFactory
+     *
+     * org.springframework.context.support.AbstractRefreshableApplicationContext#getBeanFactory()
+     * 具体的实现上，beanFactory是一个组合的属性
+     * org.springframework.context.support.AbstractRefreshableApplicationContext#beanFactory
+     * 在上下文里面是一种组合：将BeanFactory组合成类的一个属性Field，并没有完全的靠extends来获取BeanFactory
+     * 同时在接口里又是extends关系，见上面记录的层层extends或implements
+     * 这种组合就有点像代理的方式
+     *
+     * org.springframework.context.support.AbstractApplicationContext#getBean(java.lang.Class)
+     * 再来随便看一下getBean的实现，是getBeanFactory().getBean的方式获取BeanFactory
+     * 用代理的方式去查找BeanFactory，而不是作为BeanFactory的继承或实现，类自身就具备这个能力，而是用的组合对象去做事情
+     *
+     * BeanFactory和ApplicationContext是同一种类，具体实现上，ApplicationContext组合了一个BeanFactory的实现
+     * 这就是为什么ApplicationContext不等于BeanFactory，它们是复用了同一个接口，但是是两个对象
+     * @param userRepository
+     * @param beanFactory
+     */
+//    private static void whoIsIocContainer(UserRepository userRepository, BeanFactory beanFactory) {
+    private static void whoIsIocContainer(UserRepository userRepository, ApplicationContext applicationContext) {
+        //这个表达式为什么不会成立？
+        System.out.println(userRepository.getBeanFactory() == applicationContext);
+
+        //Application is a BeanFactory
+    }
 }
